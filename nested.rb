b063e923937e1c4a37078678faaae65d4edc6b55
Bri Turner <blturner3527@gmail.com>
2020-01-09 21:12:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ programmer_hash[:yukihiro_matsumoto] = {
          :known_for => "Ruby",
          :languages => ["LISP", "C"]
         }
-puts programmer_hash
+programmer_hash
 end
 puts adding_matz
 
